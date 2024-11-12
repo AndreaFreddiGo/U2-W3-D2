aceefba17fde3132e5e3e83495d3f91b88ce26a4
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2024-11-12 19:02:48 +0200

    Adds adjustments

--- exercises.js
+++ exercises.js
@@ -14,7 +14,7 @@ saveButton.addEventListener('click', function () {
   // ora vado a salvare l'attuale contenuto del input field nel localStorage
   // in una chiave di nome "user-memorized"
   localStorage.setItem(key, userName.value)
-  alert('Contenuto salvato')
+
   input.value = '' // assegno un nuovo valore alla textarea di stringa vuota
 })
 
@@ -22,10 +22,11 @@ cancelButton.addEventListener('click', function () {
   // ora vado a cancellare il nome contenuto nel localStorage
   // nella chiave di nome "user-memorized"
   localStorage.removeItem(key)
-  alert('Contenuto cancellato')
+
+  info[0].classList.add('d-none')
 })
 
-const info = document.getElementsByTagName('span')[0]
+const info = document.getElementsByClassName('info')
 const oldName = document.getElementById('oldUserName')
 
 const savedContent = localStorage.getItem(key)
@@ -35,8 +36,18 @@ const savedContent = localStorage.getItem(key)
 // nel caso sia null, non succede nulla, altrimenti
 //   visualizzo il nome già presente
 if (savedContent) {
-  info.classList.remove('d-none')
+  info[0].classList.remove('d-none')
   const old = document.createElement('span')
   old.innerText = savedContent
   oldName.appendChild(old)
 }
+
+// recupero il riferimento al counter
+document.addEventListener('DOMContentLoaded', function () {
+  const counter = document.getElementById('time')
+  let seconds = parseInt(counter.innerText)
+  setInterval(function () {
+    seconds++
+    counter.innerText = seconds
+  }, 1000)
+})
